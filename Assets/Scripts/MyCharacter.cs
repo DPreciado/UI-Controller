d852a48f809ffdb9fef3fcfb8a0b567a4c27a6f0
David <47166040+FootlessCat985@users.noreply.github.com>
2019-04-30 18:55:09 -0700

    ahí va
    primero pasos

---
+++ Assets/Scripts/MyCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacter : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    float ColorPower = 1f;
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        render.material.SetFloat("_ColorPower", ColorPower);
+    }
+}
