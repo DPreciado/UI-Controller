4d04f72eb812cdcff19dac0f9c6928fb01fb8229
David <47166040+FootlessCat985@users.noreply.github.com>
2019-04-30 19:13:22 -0700

    ya quedó
    completamos el ejemplo

--- Assets/Scripts/MyCharacter.cs
+++ Assets/Scripts/MyCharacter.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MyCharacter : MonoBehaviour
 {
     Renderer render;
     [SerializeField]
-    float ColorPower = 1f;
-    void Start()
+    float colorPower = 1f;
+
+    [SerializeField]
+    Slider sld_colorPower;
+
+    private void Start()
     {
         render = GetComponent<Renderer>();
     }
 
-    void Update()
+    private void Update()
     {
-        render.material.SetFloat("_ColorPower", ColorPower);
+        render.material.SetFloat("_ColorPower", sld_colorPower.value);
     }
 }
